26ad34a13eaf301a1e2f95400f97ed1705c78669
Quang Dat Le <quangdatlecz@gmail.com>
2021-08-08 23:30:26 +0200

    WIP: configure name for chat

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,29 @@
-fetch("https://czechichat.herokuapp.com/api/list-messages")
-	.then((response) => response.json())
-	.then((data) => console.log(data))
+class apiChat {
+  constructor() {
+    fetch("https://czechichat.herokuapp.com/api/list-messages")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+
+    this.getName()
+  }
+
+  getName = () => {
+    let setName = this.getLocalStorageName()
+
+    while (setName == null || setName == "") {
+      setName = prompt("Please enter your name/nickname.")
+    }
+
+    this.setName(setName)
+
+    return getLocalStorageName()
+  }
+
+  setName = (name) => {
+    localStorage.setItem("name", name)
+  }
+
+  getLocalStorageName = () => localStorage.getItem("name")
+}
+
+new apiChat()
